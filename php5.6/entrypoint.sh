fad38c01a59bc6b13bf7a3da1a848cff9225b962
Jordi Martin <jordimartin@gmail.com>
2019-01-15 13:15:05 +0100

    Small docker improvments

--- php5.6/entrypoint.sh
+++ php5.6/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 envsubst '${NGINX_PORT},${MONGO_PORT}' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default
-/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
